4c1cc80ccf3a5c903cdcd75117e091a421c4b311
raisyasal <124250125@student.upnyk.ac.id>
2026-04-21 19:38:02 +0700

    buat koneksi

---
+++ koneksi.php
@@ -0,0 +1,17 @@
+<?php 
+
+$hostname = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'db_cicilan';
+$port = 3306;
+
+$koneksi = new mysqli($hostname, $username, $password, $database, $port);
+if($koneksi->connect_error) {
+    die("Koneksi gagal!");
+} else {
+ echo "Koneksi berhasil";
+}
+   
+
+?>
